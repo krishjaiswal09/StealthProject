f1804af1015af841396e01f1c58797cd41a69f18
Krish Jaiswal <117284024+krishjaiswal09@users.noreply.github.com>
2025-05-14 20:01:41 +0530

    Update config.js
    The mongoDb password and jwt key is hidden

--- backend/config/config.js
+++ backend/config/config.js
@@ -1,7 +1,7 @@
 export default {
   PORT: process.env.PORT || 5050,
-  MONGODB_URI: process.env.MONGODB_URI || 'mongodb+srv://krishjaiswal67890:OBK2i9qT4eie1Hv2@cluster0.4bzzc6t.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
-  JWT_SECRET: process.env.JWT_SECRET || 'secretkey123456789',
+  MONGODB_URI: process.env.MONGODB_URI || 'mongodb+srv://krishjaiswal67890:@cluster0.4bzzc6t.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
+  JWT_SECRET: process.env.JWT_SECRET || '',
   JWT_EXPIRES_IN: process.env.JWT_EXPIRES_IN || '30d',
   NODE_ENV: process.env.NODE_ENV || 'development',
   IS_PRODUCTION: process.env.NODE_ENV === 'production',
